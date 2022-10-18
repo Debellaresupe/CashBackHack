f3ed7e75aec2f1a4b9dcae36dd23f5d1ee8bfa61
Elnar KIlmukhametov <soulmiker@gmail.com>
2022-10-18 14:06:22 +0500

    add junit4-platform

--- src/test/java/ru/netology/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CashBackHackServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 public class CashBackHackServiceTest {
@@ -23,6 +25,31 @@ public class CashBackHackServiceTest {
     }
     @org.junit.Test
     public void testRemain1100() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldBeRemain100() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldBeRemain0() {
+        CashBackHackService service = new CashBackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldBeRemain900() {
         CashBackHackService service = new CashBackHackService();
         int amount = 1100;
         int expected = 900;
